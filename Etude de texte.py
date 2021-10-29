aeda06e0ad25ead42db4bc3affadcb801630d83a
Aurelie <aurelie.chopardlallier@gmail.com>
2021-10-29 10:14:56 +0200

    ajout d'un txt Miserables et d'un py

---
+++ Etude de texte.py
@@ -0,0 +1,23 @@
+contenu = "Formes que prend la souffrance pendant le sommeil\nTrois heures du matin venaient de sonner, et il y avait cinq heures
+qu'il marchait ainsi, presque sans interruption lorsqu'il se laissa
+tomber sur sa chaise.
+
+Il s'y endormit et fit un rêve."
+print(contenu)
+
+# ## Gérer les caractères spéciaux
+# for carac in contenu :
+#     if carac == "Ã©" :
+#         contenu.replace(carac, "é")
+# print(contenu)
+
+# ## Générer la matrice de transition
+# import numpy as np
+# matrice = np.array(26*[26*[0]])
+# print(matrice)
+
+# for i in range(len(contenu)) :
+#     matrice[i]
+
+ch = "Bonjour les enfants\nil est l'heure"
+print(str(ch))
