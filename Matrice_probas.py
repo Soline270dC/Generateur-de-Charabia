49fa0db46c44a5c2d9b265f31893c038021e768e
Soline270dC <soline.ducrest@psl.eu>
2021-11-04 18:37:35 +0100

    ajout de commentaire sur le code python + mise à jour du README

--- Matrice_probas.py
+++ Matrice_probas.py
@@ -4,21 +4,33 @@ import numpy as np
 import codecs
 
 nom = input("nom du ficher avec chemin d'accès absolu : ")
-
+# lecture du fichier
 f = codecs.open(nom, 'r', 'utf-8')
 texte = f.read()
 f.close()
 
-for c in '?,.;:!()-' :
+# on efface la ponctuation
+for c in '?,.;:!()' :
     texte = texte.replace(c, '')
 
+# on enlève les sauts de ligne et on baisse la casse
 texte = texte.replace('\n', ' ')
 texte = texte.lower()
 
-ch = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûü \'-'
+# ch correspond à l'ordre des caractères dans la matrice de transition
+# J'ai inclus l'apostrophe et le tiret (mots composés)
+ch = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
 
+# on crée une matrice de zéros
 Mat = np.zeros((len(ch), len(ch)))
 
+# on garde en note l'indice de la ligne (lettre initiale)
+# puis on cherche l'indice de la colonne (lettre suivante)
+# on implémente la matrice à la case ind_l, ind_c
+# l'indice de la colonne devient celiu de la ligne
+# (on considère maintenant cette lettre comme lettre initiale)
+# au début, on met l'indice de la ligne sur espace pour compter le début du mot
+# on fait de même à la fin pour compter la fin du mot
 ind_l = ch.index(' ')
 for i in range(len(texte)-1) :
     ind_c = ch.index(texte[i])
@@ -27,6 +39,8 @@ for i in range(len(texte)-1) :
 ind_c = ch.index(' ')
 Mat[ind_l][ind_c] += 1
 
+# maintenant que la matrice contient le nombre de transitions,
+# on la ramène à une matrice stochastique en divisant chaque ligne par la somme des lignes
 for i in range(len(ch)) :
     s = sum(Mat[i])
     if s != 0 :
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Generateur-de-Charabia
 
+## Utilisation de la matrice de transition
 La matrice de probabilités générée dans mon programme a pour lignes la lettre et pour colonnes la probabilité de ce qui suit.
 Afin d'obtenir la ligne, il faut multiplier une matrice ligne par cette matrice.
+liste des caractères dans l'ordre : abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç '-
+
+Afin de réutiliser la matrice dans un programme ultérieur, il faut à mon avis créer une fonction qui génère la matrice de transition à partir d'un fichier donné en argument. On peut ensuite importer cette fonction dans notre fichier car il semble difficile de stocker la matrice.
+
+## Génération de la matrice de transition
+On se positionne sur la première lettre du texte et on regarde quelle lettre la suit.
+On implémente la case de la matrice correspondant à cette transition (cf plus huat pour déterminer ça).
+La deuxième lettre (qualifiée auparavant de 'lettre qui suit') prend la place de la première lettre et on regarde sa propre successeure.
+On continue jusqu'à arriver à la fin du texte.
+Les deux extrêmes sont gérés en ajoutant symboliquement un espace au début et à la fin
+
+On rend la matrice stochastique en divisant chaque ligne par la somme.
+
+Problème :
+Si on ne croise aucune occurence d'une lettre et qu'on a du coup une ligne nulle, est-ce qu'on peut vraiment parler de matrice stochastique ?
+Il faudrait donc faire attention à n'avoir que des caractères spéciaux qu'on rencontrera ?
