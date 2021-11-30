de0561023879521aee4af6bddb5da2394f116b2a
Soline270dC <soline.ducrest@psl.eu>
2021-11-30 17:35:34 +0100

    fin du générateur de mots

--- createur_mot.py
+++ createur_mot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 Mat = np.load('Matrice_probas.npy')
 
@@ -11,4 +12,12 @@ for _ in range(n) :
     mot = ''
     i = 38 # = alphabet.index(' ')
     car = ''
-    
+    while car != ' ' : # on s'arrête quand on arrive à la fin d'un mot (signalé par ' ')
+        car = random.choices(alphabet, Mat[i])[0] # random.choices renvoie une liste de 1 élément
+        mot += car
+        i = alphabet.index(car)
+    Mots.append(mot.replace(' ','\n')) # on remplace l'espace final par un passage à la ligne
+
+f = open(f'liste_{n}_mots.txt', 'w')
+f.writelines(Mots)
+f.close()
