8e96b27df1be0ddef1599b3a7b8c345e654b0979
Soline270dC <soline.ducrest@psl.eu>
2021-11-30 17:42:35 +0100

    génération de mots bizarres : dakebonit

--- createur_mot.py
+++ createur_mot.py
@@ -12,11 +12,11 @@ for _ in range(n) :
     mot = ''
     i = 38 # = alphabet.index(' ')
     car = ''
-    while car != ' ' : # on s'arrête quand on arrive à la fin d'un mot (signalé par ' ')
-        car = random.choices(alphabet, Mat[i])[0] # random.choices renvoie une liste de 1 élément
+    while car != ' ' :
+        car = random.choices(alphabet, Mat[i])[0]
         mot += car
         i = alphabet.index(car)
-    Mots.append(mot.replace(' ','\n')) # on remplace l'espace final par un passage à la ligne
+    Mots.append(mot.replace(' ','\n'))
 
 f = open(f'liste_{n}_mots.txt', 'w')
 f.writelines(Mots)
