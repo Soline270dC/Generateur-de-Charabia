067ab702c7255ae2ef4f566d213c9bbf3e764e94
Soline270dC <soline.ducrest@psl.eu>
2021-12-01 12:41:40 +0100

    ajout des lettres non utilisée, etc.

--- heatmap.py
+++ heatmap.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pylab as plb
 import seaborn as sns
 
-Mat = np.load('Matrice_probas-2.npy')
+Mat = np.load('Matrice_probas.npy')
+alphabet = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
+
 plb.style.use('seaborn')
 plb.figure(figsize = (10,10))
 heat_map = sns.heatmap(Mat, cmap = 'gnuplot')
