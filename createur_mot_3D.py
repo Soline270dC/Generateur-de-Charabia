0b43d8f790b8dfd68e25f89701bcc5186c5b74f7
Soline270dC <soline.ducrest@psl.eu>
2021-12-10 15:56:20 +0100

    ORGANISATION + ajout du programme en 3D

---
+++ createur_mot_3D.py
@@ -0,0 +1,23 @@
+import numpy as np
+import random
+
+Mat = np.load('Matrice_probas-2.npy')
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
+
+n = int(input('nombre de mots à créer : '))
+Mots = []
+
+for _ in range(n) :
+    mot = ''
+    i = 38 # = alphabet.index(' ')
+    car = ''
+    while car != ' ' :
+        car = random.choices(alphabet, Mat[i])[0]
+        mot += car
+        i = alphabet.index(car)
+    Mots.append(mot.replace(' ','\n'))
+
+f = open(f'liste_{n}_mots.txt', 'w')
+f.writelines(Mots)
+f.close()
---
+++ mat_trans_3D.py
@@ -0,0 +1,40 @@
+import numpy as np
+import codecs
+
+nom = input('nom du fichier + extension : ')
+f = codecs.open(nom, 'r', 'utf-8') # utf-8 à adapter en fonction du document
+texte = f.read()
+f.close()
+
+# fonction 'propre' (cf origin/Aurelie)
+for c in '"(),?;.:!' : # cas des apostrophes à aviser ("c'", "l'", "d'")
+    texte = texte.replace(c,'')
+
+for c in '\t\n' :
+    texte = texte.replace(c, ' ')
+
+texte = texte.lower()
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
+
+Mat = np.zeros((len(alphabet), len(alphabet)))
+List_occ = [0]*len(alphabet)
+
+# création de matrice et remplissage des occurences
+ind_l = 38 # = alphabet.index(' ')
+for i in range(len(texte)-1) :
+    car = texte[i]
+    if car in alphabet :
+        ind_c = alphabet.index(car)
+        Mat[ind_l][ind_c] += 1
+        List_occ[ind_c] += 1
+        ind_l = ind_c
+ind_c = 38
+Mat[ind_l][ind_c] += 1
+
+# normalisation
+for i in range(len(alphabet)) :
+    if List_occ[i] != 0 :
+        Mat[i] = Mat[i]/List_occ[i]
+
+np.save('Matrice_probas', Mat)
