8ed23964e45cfe8d01b8826629fc84247bbf7404
Soline270dC <soline.ducrest@psl.eu>
2022-01-02 10:24:35 +0100

    modif créateur de la liste de mots français

--- Bases_de_donnees/ensemble_mots.py
+++ Bases_de_donnees/ensemble_mots.py
@@ -1,32 +1,26 @@
-import numpy as np
 import codecs
 
-f = codecs.open(r'Bases_de_donnees\Les_miserables_1.txt', 'r', 'utf-8') # utf-8 à adapter en fonction du document
+f = codecs.open(r'Bases_de_donnees\Les_miserables_1.txt', 'r', 'utf-8')
 texte = f.read()
 f.close()
 
-# fonction 'propre' (cf origin/Aurelie)
-for c in ['"','(',')',',','?',';','.',':','!','--','_','»','«'] : # cas des apostrophes à aviser ("c'", "l'", "d'")
+for c in ['"','(',')',',','?',';','.',':','!','--','_','»','«'] :
     texte = texte.replace(c,' ')
 texte = texte.lower()
 
-mots_mis = texte.split()
-Ens_mots_mis = set(mots_mis)
+mots_misérables = texte.split()
+Ens_mots_misérables = set(mots_misérables)
 
-f = codecs.open(r'Bases_de_donnees\Liste_mots.txt', 'r', 'utf-8') # utf-8 à adapter en fonction du document
+f = codecs.open(r'Bases_de_donnees\Liste_mots.txt', 'r', 'utf-8')
 texte = f.read()
 f.close()
 
 texte = texte.lower()
 
-mots_list = texte.split()
-Ens_mots_list = set(mots_list)
+mots_liste = texte.split()
+Ens_mots_liste = set(mots_liste)
 
-Ens_mots = Ens_mots_mis|Ens_mots_list
-
-f = codecs.open('set_ensemble_mots.txt', 'w', 'utf-8')
-f.write(repr(Ens_mots))
-f.close()
+Ens_mots = Ens_mots_misérables|Ens_mots_liste
 
 Liste = sorted(list(Ens_mots))
 liste = '\n'.join(Liste)
