b9bfd8c3084c3fffeda021789cea995c508a7b85
Lise Depinay <lise.depinay@psl.eu>
2022-01-05 18:09:46 +0100

    algorithme commenté

--- 2D/createur_mot_2D.py
+++ 2D/createur_mot_2D.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+# Importation de la matrice de probabilté de la base de donnée choisi nous permettant de générer des mots nouveaux
 Mat = np.load('Matrice_probas-2.npy')
 
 alphabet = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
@@ -8,9 +9,10 @@ alphabet = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
 n = int(input('nombre de mots à créer : '))
 Mots = []
 
+# Implémentation du générateur de nouveaux mots
 for _ in range(n) :
     mot = ''
-    i = 38 # = alphabet.index(' ')
+    i = 38 # Initialisation au caractère "espace" pour la première lettre (= alphabet.index(' '))
     car = ''
     while car != ' ' :
         car = random.choices(alphabet, Mat[i])[0]
@@ -18,6 +20,7 @@ for _ in range(n) :
         i = alphabet.index(car)
     Mots.append(mot.replace(' ','\n'))
 
+#Création d'un fichier contenant la liste de mot généré
 f = open(f'liste_{n}_mots_2D.txt', 'w')
 f.writelines(Mots)
 f.close()
