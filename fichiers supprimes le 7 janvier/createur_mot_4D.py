38a509210572d1495b0ab47df719035f366885c5
AurelieCL2021 <93125251+AurelieCL2021@users.noreply.github.com>
2022-01-07 22:51:45 +0100

    ajout des fichiers supprimes le 7 janvier

---
+++ fichiers supprimes le 7 janvier/createur_mot_4D.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+
+# Importation de la matrice de probabilté de la base de donnée choisi nous permettant de générer des mots nouveaux
+Mat = np.load('Matrice_probas_4D_Miserables_tot.npy')
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzàâéèêëîïôûüç \'-'
+
+n = int(input('nombre de mots à créer : '))
+Mots = []
+
+# Implémentation du générateur de nouveaux mots
+for _ in range(n) :
+    mot = ''
+    x = y = z = 38 # Initialisation des termes au caractère "espace" pour la première lettre (= alphabet.index(' '))
+    car = ''
+    while car != ' ' :
+        car = random.choices(alphabet, Mat[x][y][z])[0]
+        mot += car
+        x = y
+        y = z
+        z = alphabet.index(car)
+    Mots.append(mot.replace(' ','\n'))
+
+#Création d'un fichier contenant la liste de mot généré
+f = open(f'liste_{n}_mots_4D.txt', 'w')
+f.writelines(Mots)
+f.close()
